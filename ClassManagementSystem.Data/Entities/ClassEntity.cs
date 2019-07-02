6c509bdd72706280c67166938946e552ad49d52c
Yulin Ye <ericye1001@gmail.com>
2019-07-02 23:44:49 +1000

    Add appsettings.json for ClassManagementSystem.Data
    Add appsettings.json for ClassManagementSystem.Data

---
+++ ClassManagementSystem.Data/Entities/ClassEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassManagementSystem.Data.Entities
+{
+    public class ClassEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
--- ClassManagementSystem.Data/PostgresSQL/PostgresSQLDataContext.cs
+++ ClassManagementSystem.Data/PostgresSQL/PostgresSQLDataContext.cs
@@ -12,20 +12,7 @@ namespace ClassManagementSystem.Data.PostgresSQL
         {
         }
         public DbSet<StudentEntity> Students { get; set; }
-        /*
-        public DbSet<AnimalEntity> Animals { get; set; }
-        public DbSet<PersonEntity> Persons { get; set; }
-        public DbSet<FriendShipEntity> FriendShip { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<FriendShipEntity>()
-                .HasIndex(friend => friend.FromPerson);
-            modelBuilder.Entity<FriendShipEntity>()
-                .HasIndex(friend => friend.ToPerson);
-            base.OnModelCreating(modelBuilder);
-        }
-        */
+        public DbSet<ClassEntity> Classes { get; set; }
     }
 }
 
